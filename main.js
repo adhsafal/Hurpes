db5787a9858669395919603e5b26e0b41c33a44c
Safal <mr.safaladh@gmail.com>
2022-03-03 20:49:35 +0545

    Working on home page

---
+++ main.js
@@ -0,0 +1,25 @@
+$('.banner_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // dots: true,
+    // cssEase: 'linear',
+    autoplay: true,
+    // autoplaySpeed: 400,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-angle-left left_arrow"></i>',
+    nextArrow: '<i class = "fa-solid fa-angle-right right_arrow"></i>',
+});
+
+$('.program_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    // cssEase: 'linear',
+    autoplay: true,
+    // autoplaySpeed: 400,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-angle-left left_arrow"></i>',
+    nextArrow: '<i class = "fa-solid fa-angle-right right_arrow"></i>',
+});
